3b5317406a77b6080bb59d14f14c4e8e541e7970
Dabo Ross <daboross@daboross.net>
2014-09-20 23:00:46 -0700

    Simplify Main.hx a lot

--- Color/src/main/haxe/net/daboross/hex/Main.hx
+++ Color/src/main/haxe/net/daboross/hex/Main.hx
@@ -1,25 +1,11 @@
 package net.daboross.hex;
 
-import createjs.easeljs.SpriteSheet;
-import createjs.easeljs.Shape;
-import createjs.easeljs.Stage;
-import createjs.easeljs.Sprite;
-import createjs.easeljs.Ticker;
-
 import net.daboross.hex.SpaceHandler;
 
 class Main {
 
-    private var space:SpaceHandler = new SpaceHandler();
-
-    public function new() {
-    }
-
     public static function main() {
-        new Main().run();
-    }
-
-    public function run() {
+        var space:SpaceHandler = new SpaceHandler();
         space.start();
     }
 }
